3cfdee37375a191913184a0efb6ff4bcbc30163c
rurtubiac <randy.urtubia@inacapmail.cl>
2016-09-24 04:49:06 -0300

    Initial project started

---
+++ src/hackerrank/helloworld/HackerrankHelloworld.java
@@ -0,0 +1,27 @@
+/*
+ *  This work by Randy Urtubia is licensed under the Creative Commons.
+ * 
+ *  Attribution: You must give appropriate credit, provide a link to the license, and indicate if changes were made.
+ *    You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+ *  NonCommercial: You may not use the material for commercial purposes.
+ *  ShareAlike: If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.
+ * 
+ *  Based on a work at http://www.notnull.cl.
+ *  To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/.
+ */
+package hackerrank.helloworld;
+
+/**
+ *
+ * @author Randy
+ */
+public class HackerrankHelloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
